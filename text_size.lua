e42ccbc28815bafd480a2bc26043e8b8f40cd03a
AXOMAT1C <83140080+AXOMAT1C@users.noreply.github.com>
2025-10-17 00:52:42 +0200

    Update text_size.lua

--- text_size.lua
+++ text_size.lua
@@ -1,13 +1,8 @@
 -- textsize.lua
--- Verwaltet Balkenlängen für Desktop und Tablet
-local monitor = peripheral.find("monitor") -- falls kein spezielles Side angegeben
+local monitor = peripheral.find("monitor")
 local w,h = 51,19
+if monitor then w,h = monitor.getSize() end
 
-if monitor then
-    w,h = monitor.getSize()
-end
-
--- Balkenlängen
 local sizes = {
     desktop = math.min(30, w-2),
     tablet  = math.min(20, w-2)
