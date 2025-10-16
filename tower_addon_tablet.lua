e130c5755e99b932b4ee1d8532ace019aabd2acd
AXOMAT1C <83140080+AXOMAT1C@users.noreply.github.com>
2025-10-17 00:53:53 +0200

    Update tower_addon_tablet.lua

--- tower_addon_tablet.lua
+++ tower_addon_tablet.lua
@@ -1,25 +1,14 @@
 -- tower_addon_tablet.lua
 local monitor = peripheral.wrap("top")
-local w,h = monitor.getSize()
-monitor.clear()
-monitor.setBackgroundColor(colors.black)
-monitor.setTextColor(colors.white)
-
 local tower_state = require("tower_state")
+local textsize = require("textsize")
+
 local floors = {"DACH","WOOD","FARMS","ITEM","DEFENSE","STORAGE","EN/ME"}
-local tabletBarLength = math.min(20, w-2)
+local colorsFloor = {DACH=colors.lightBlue,WOOD=colors.brown,FARMS=colors.green,ITEM=colors.yellow,DEFENSE=colors.red,STORAGE=colors.orange,["EN/ME"]=colors.purple}
+
+local tabletBarLength = textsize.getBarLength("tablet")
 
 rednet.open("back")
-
-local colorsFloor = {
-    DACH = colors.lightBlue,
-    WOOD = colors.brown,
-    FARMS = colors.green,
-    ITEM = colors.yellow,
-    DEFENSE = colors.red,
-    STORAGE = colors.orange,
-    ["EN/ME"] = colors.purple
-}
 
 local function drawTabletUI()
     monitor.clear()
@@ -32,15 +21,15 @@ local function drawTabletUI()
         monitor.setBackgroundColor(colorsFloor[floor] or colors.white)
         monitor.write(string.rep(" ", filled))
         monitor.setBackgroundColor(colors.black)
-        monitor.write(string.rep("-", tabletBarLength - filled) .. " " .. floor .. string.format(" %.0f%%", progress*100))
+        monitor.write(string.rep("-", tabletBarLength-filled) .. " " .. floor .. string.format(" %.0f%%", progress*100))
     end
 end
 
 while true do
     local senderId, message = rednet.receive()
-    if message.type == "update" then
+    if message.type=="update" then
         for floor,value in pairs(message.data) do
-            tower_state.setProgress(floor, value)
+            tower_state.setProgress(floor,value)
         end
     end
     drawTabletUI()
