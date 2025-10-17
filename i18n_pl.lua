4e36e0aebe216e99126314305388c47f7df87443
AXOMAT1C <83140080+AXOMAT1C@users.noreply.github.com>
2025-10-17 02:00:59 +0200

    Create i18n_pl.lua

---
+++ i18n_pl.lua
@@ -0,0 +1,16 @@
+return {
+    title="Postęp Wieży",
+    floor="Piętro",
+    button_plus="+",
+    button_minus="-",
+    floors={"DACH","WOOD","FARMS","ITEM","DEFENSE","STORAGE","EN/ME"},
+    tasks={
+        DACH={"Zbuduj teleporter","Aktywuj End Access","Dekoracyjny dach"},
+        WOOD={"Zbierz drewno","Zasadź drzewa","Dekoracyjne bloki drewniane"},
+        FARMS={"Ustaw roślinę 1","Dodaj zwierzęta 1","Zbuduj akwakulturę"},
+        ITEM={"Skonfiguruj terminal AE2","Zbuduj przenośniki"},
+        DEFENSE={"Umieść wieżyczki","Wzmocnij mury"},
+        STORAGE={"Ustaw szuflady","Zbuduj bufory"},
+        ["EN/ME"]={"Zbuduj generator mocy","Skonfiguruj AE2 Core"}
+    }
+}
