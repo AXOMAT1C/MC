6ce14e28cea623220381f8e3c108526941ee61ac
AXOMAT1C <83140080+AXOMAT1C@users.noreply.github.com>
2025-10-17 02:02:46 +0200

    Update tower_state.lua

--- tower_state.lua
+++ tower_state.lua
@@ -1,23 +1,42 @@
--- tower_state.lua
-local state = {}
-local progress = {}
+local stateFile = "tower_state.dat"
 
-local function setProgress(floor, value)
-    progress[floor] = math.min(math.max(value,0),1)
-end
-
-local function getProgress(floor)
-    return progress[floor] or 0
-end
-
-local function getAll()
-    local copy = {}
-    for k,v in pairs(progress) do copy[k]=v end
-    return copy
-end
-
-return {
-    setProgress = setProgress,
-    getProgress = getProgress,
-    getAll = getAll
+local state = {
+    lang = "de",
+    progress = {} -- z.B. {DACH=0.5, WOOD=0.2,...}
 }
+
+-- Lade State
+local function load()
+    if fs.exists(stateFile) then
+        local f = fs.open(stateFile,"r")
+        state = textutils.unserialize(f.readAll()) or state
+        f.close()
+    end
+end
+
+-- Speichere State
+local function save()
+    local f = fs.open(stateFile,"w")
+    f.write(textutils.serialize(state))
+    f.close()
+end
+
+-- Set Progress
+function state.setProgress(floor,val)
+    state.progress[floor] = math.min(math.max(val,0),1)
+    save()
+end
+
+-- Get Progress
+function state.getProgress(floor)
+    return state.progress[floor] or 0
+end
+
+-- Set Language
+function state.setLang(l)
+    state.lang = l
+    save()
+end
+
+load()
+return state
