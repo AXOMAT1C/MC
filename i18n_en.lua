9dd827bf2166d71d4a7ae740a975e7d333ae73d2
AXOMAT1C <83140080+AXOMAT1C@users.noreply.github.com>
2025-10-17 03:25:22 +0200

    Update i18n_en.lua

--- i18n_en.lua
+++ i18n_en.lua
@@ -1,25 +1,19 @@
 local T = {}
 
-T.floors = {
-    "ROOF",
-    "WOOD",
-    "FARMS",
-    "ITEM",
-    "DEFENSE",
-    "STORAGE",
-    "EN_ME"
-}
-
-T.tasks = {
-    ["ROOF"] = {"Build Teleporter", "Set End Access", "Build Decorative Roof"},
-    ["WOOD"] = {"Place Logs", "Plant Trees", "Place Decorative Blocks"},
-    ["FARMS"] = {"Plant 1", "Plant 2", "Harvest Crops"},
-    ["ITEM"] = {"Create Conveyors", "Place AE2 IO-Terminals", "Build Crafting Station"},
-    ["DEFENSE"] = {"Place Turrets", "Build Walls"},
-    ["STORAGE"] = {"Setup Drawers", "Place Functional Storage"},
-    ["EN_ME"] = {"Build Powah Generator", "Setup AE2 Core", "Place ME Storage Cells"}
-}
-
+T.title = "Tower Management"
+T.floor = "Floor"
 T.button_plus = "+"
 T.button_minus = "-"
+
+T.floors = {"Roof","Wood","Farms","Item","Defense","Storage","EN/ME"}
+T.tasks = {
+    Roof = {"Build Teleporter","Install Glass Roof","Decorative Roof"},
+    Wood = {"Collect Logs","Plant Trees","Set Decorations"},
+    Farms = {"Plant Crops","Care Animals","Bee Keeping"},
+    Item = {"AE2 Terminals","Crafting Station","Build Conveyors"},
+    Defense = {"Build Turrets","Build Walls"},
+    Storage = {"Setup Drawers","Buffers","Intermediate Storage"},
+    ["EN/ME"] = {"Build Powah Generator","Setup AE2 Core","Setup ME Storage"}
+}
+
 return T
