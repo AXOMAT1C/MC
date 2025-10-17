c080d57a2de4f825b6e2aceeff56632eda043847
AXOMAT1C <83140080+AXOMAT1C@users.noreply.github.com>
2025-10-17 02:00:00 +0200

    Create i18n_de.lua

---
+++ i18n_de.lua
@@ -0,0 +1,16 @@
+return {
+    title="Tower Fortschritt",
+    floor="Etage",
+    button_plus="+",
+    button_minus="-",
+    floors={"DACH","WOOD","FARMS","ITEM","DEFENSE","STORAGE","EN/ME"},
+    tasks={
+        DACH={"Teleporter bauen","End Access aktivieren","Dekoratives Dach"},
+        WOOD={"Logs sammeln","Bäume pflanzen","Dekorative Holzblöcke"},
+        FARMS={"Pflanzen 1 setzen","Tiere 1 einfügen","Aquakultur bauen"},
+        ITEM={"AE2 Terminal einrichten","Förderbänder bauen"},
+        DEFENSE={"Turrets platzieren","Wände verstärken"},
+        STORAGE={"Drawers einrichten","Buffers bauen"},
+        ["EN/ME"]={"Powah Generator bauen","AE2 Core aufstellen"}
+    }
+}
