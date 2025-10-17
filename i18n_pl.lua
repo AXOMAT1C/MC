a3af87beba9393c7334292925150bf7664ed8032
AXOMAT1C <83140080+AXOMAT1C@users.noreply.github.com>
2025-10-17 02:38:28 +0200

    Update i18n_pl.lua

--- i18n_pl.lua
+++ i18n_pl.lua
@@ -1,16 +1,25 @@
-return {
-    title="Postęp Wieży",
-    floor="Piętro",
-    button_plus="+",
-    button_minus="-",
-    floors={"DACH","WOOD","FARMS","ITEM","DEFENSE","STORAGE","EN/ME"},
-    tasks={
-        DACH={"Zbuduj teleporter","Aktywuj End Access","Dekoracyjny dach"},
-        WOOD={"Zbierz drewno","Zasadź drzewa","Dekoracyjne bloki drewniane"},
-        FARMS={"Ustaw roślinę 1","Dodaj zwierzęta 1","Zbuduj akwakulturę"},
-        ITEM={"Skonfiguruj terminal AE2","Zbuduj przenośniki"},
-        DEFENSE={"Umieść wieżyczki","Wzmocnij mury"},
-        STORAGE={"Ustaw szuflady","Zbuduj bufory"},
-        ["EN/ME"]={"Zbuduj generator mocy","Skonfiguruj AE2 Core"}
-    }
+local T = {}
+
+T.floors = {
+    "DACH",
+    "WOOD",
+    "FARMS",
+    "ITEM",
+    "DEFENSE",
+    "STORAGE",
+    "EN_ME"
 }
+
+T.tasks = {
+    ["DACH"] = {"Zbuduj teleporter", "Ustaw End Access", "Zbuduj dekoracyjny dach"},
+    ["WOOD"] = {"Umiesc klocki", "Zasadz drzewa", "Umiesc bloki dekoracyjne"},
+    ["FARMS"] = {"Zasadz roslina1", "Zasadz roslina2", "Zbierz plony"},
+    ["ITEM"] = {"Stworz przenosniki", "Umiesc AE2 IO-Terminals", "Zbuduj Crafting Station"},
+    ["DEFENSE"] = {"Umiesc wiezyczki", "Zbuduj mury"},
+    ["STORAGE"] = {"Skonfiguruj Drawers", "Umiesc Functional Storage"},
+    ["EN_ME"] = {"Zbuduj Powah Generator", "Skonfiguruj AE2 Core", "Umiesc ME Storage Cells"}
+}
+
+T.button_plus = "+"
+T.button_minus = "-"
+return T
